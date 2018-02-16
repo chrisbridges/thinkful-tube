7f1f7357d80f0829a2e632e33601bf9f9e8fb0a2
Chris Bridges <christopher_bridges@icloud.com>
2018-02-15 17:36:58 -0800

    api calls appear to be working at now

---
+++ app.js
@@ -0,0 +1,43 @@
+const YOUTUBE_API_ENDPOINT = "https://www.googleapis.com/youtube/v3/search";
+
+function getDataFromAPI (userSearchValue, callback) {
+	const params = {
+		part: 'snippet',
+		key: 'AIzaSyDh_xGPxAMR6QUO2UmQ9dN-hVTuh_uoXqc',
+		q: userSearchValue,
+		maxResults: 6
+	};
+	$.getJSON(YOUTUBE_API_ENDPOINT, params, callback);
+}
+
+function renderResult (item) {
+	const videoIDprecursor = "https://www.youtube.com/watch?v=";
+	const videoID = item.videoID;
+	return `
+		<div class='video-result'>
+			<img class="video-result-thumbnail" src="${item.snippet.thumbnails.medium.url}">
+			<a href="${videoIDprecursor + videoID}"><h2>${item.snippet.title}</h2></a>
+			<span>by</span>
+			<a href="#"><h3>${item.snippet.channelTitle}</h3></a>
+		</div>`
+}
+
+function displayResults (data) {
+	const results = data.items.map(function(item) {
+		return renderResult(item);
+	});
+	$('.search-results').html(results);
+}
+
+function listenForUserSearch () {
+	$('#youtube-search-form').submit(function(event) {
+		event.preventDefault();
+		const userSearch = $(this).find('#search-bar');
+		const userSearchValue = $(this).find('#search-bar').val();
+		userSearch.val('');
+		getDataFromAPI(userSearchValue, displayResults);
+	});
+}
+
+
+$(listenForUserSearch);
